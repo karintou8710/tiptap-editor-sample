3bf394ee6ce3c1b95b312cc05bf14a474c321ab7
karintou8710 <karintou8710@gmail.com>
2024-08-20 22:47:23 +0900

    feat: react-iconsの追加

--- src/features/editor/components/tooltips/index.tsx
+++ src/features/editor/components/tooltips/index.tsx
@@ -1,6 +1,7 @@
 import { useCurrentEditor } from "@tiptap/react";
 
 import styles from "./index.module.scss";
+import { TbH1, TbH2, TbH3 } from "react-icons/tb";
 
 export default function Tooltips() {
   const { editor } = useCurrentEditor();
@@ -14,28 +15,28 @@ export default function Tooltips() {
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 1 }).run()
           }
-          aria-role="checkbox"
+          role="checkbox"
           aria-checked={editor.isActive("heading", { level: 1 })}
         >
-          H1
+          <TbH1 size={20} />
         </button>
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 2 }).run()
           }
-          aria-role="checkbox"
+          role="checkbox"
           aria-checked={editor.isActive("heading", { level: 2 })}
         >
-          H2
+          <TbH2 size={20} />
         </button>
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 3 }).run()
           }
-          aria-role="checkbox"
+          role="checkbox"
           aria-checked={editor.isActive("heading", { level: 3 })}
         >
-          H3
+          <TbH3 size={20} />
         </button>
       </div>
     </div>
