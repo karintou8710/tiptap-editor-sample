790a5bcf289d08bfdf87bac41cef33bd9af92a22
karintou8710 <karintou8710@gmail.com>
2024-08-31 01:37:41 +0900

    feat: tweetのURLペースト対応

--- src/features/editor/exntensions/tweet-embed/index.ts
+++ src/features/editor/exntensions/tweet-embed/index.ts
@@ -1,5 +1,7 @@
-import { Node, ReactNodeViewRenderer } from "@tiptap/react";
+import { Node, nodePasteRule, ReactNodeViewRenderer } from "@tiptap/react";
 import View from "./view";
+
+const twitterUrlReg = /^https:\/\/(twitter\.com|x\.com)\/.*\/status\/(\d+)$/g;
 
 const TweetEmbed = Node.create({
   name: "tweet-embed",
@@ -34,6 +36,21 @@ const TweetEmbed = Node.create({
 
   addNodeView() {
     return ReactNodeViewRenderer(View);
+  },
+
+  addPasteRules() {
+    return [
+      nodePasteRule({
+        find: twitterUrlReg,
+        type: this.type,
+        getAttributes(match) {
+          console.log(match);
+          return {
+            tweetId: match[2],
+          };
+        },
+      }),
+    ];
   },
 });
 
