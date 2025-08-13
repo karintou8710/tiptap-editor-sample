28cb250cc7b5ccddfb71ac949addf19c1cbee389
karintou8710 <karintou8710@gmail.com>
2025-08-13 21:34:37 +0900

    feat: placeholderで選択条件を柔軟に

--- src/features/editor/components/editor/extensions.ts
+++ src/features/editor/components/editor/extensions.ts
@@ -1,7 +1,7 @@
 import { Document } from "@tiptap/extension-document";
 import { Paragraph } from "@tiptap/extension-paragraph";
 import { Text } from "@tiptap/extension-text";
-import { Placeholder } from "@tiptap/extension-placeholder";
+import { Placeholder } from "../../exntensions/functionality/placeholder";
 import { History } from "@tiptap/extension-history";
 import { Extensions } from "@tiptap/react";
 import { BulletList, ListItem, OrderedList } from "@tiptap/extension-list";
